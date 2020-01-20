c3ff7068713cc59a858d2241f332ddf783d83bd3
Alan Doyle <alandoyle@Alans-MacBook-Pro.local>
2020-01-20 22:17:36 +0000

    Updates
    New files

---
+++ pages/index.js
@@ -0,0 +1,60 @@
+import Layout from '../comps/MyLayout';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
+const Index = props => (
+    <Layout>
+        <h1>Batman shows</h1>
+        <ul>
+            {props.shows.map(show => (
+                <li key={show.id}>
+                    <Link href="/p/[id]" as={`/p/${show.id}`}>
+                        <a>{show.name}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+        <style jsx>
+        {`
+        h1, 
+        a {
+        font - family: "Arial";
+        color: #212121;
+        font-size: 22px;
+        }
+
+        ul {
+        padding: 0;
+        }
+
+        li {
+        list - style: none;
+        margin: 5px 0;
+        }
+
+        a {
+        text - decoration: none;
+        color: #757575;
+        }
+
+        a:hover {
+        opacity: 0.6;
+        }
+        `}
+        </style>
+
+    </Layout>
+);
+
+Index.getInitialProps = async function() {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await res.json();
+
+    console.log(`Show data fetched. Count: ${data.length}`);
+
+    return {
+        shows: data.map(entry => entry.show)
+    };
+};
+
+export default Index;
